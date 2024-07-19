7b65a270fa9b0823179e22d8fbc72d805d7014e9
S'Bussiso Dube <80188685+Sbussiso@users.noreply.github.com>
2024-07-19 08:11:56 +0000

    added proc file

--- application.py
+++ application.py
@@ -88,5 +88,4 @@ def chat_assistant_route():
 
 
 if __name__ == '__main__':
-    #application.run(debug=True)
-    application.run(host='0.0.0.0', port=80, debug=True)
+    application.run(host='0.0.0.0', port=8000, debug=True)
